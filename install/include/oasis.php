6dc44145548d957d52adc6ccbad47f5296c7b640
John <juaorok@hotmail.com>
2021-06-07 21:01:28 -0300

    Update oasis.php

--- install/include/oasis.php
+++ install/include/oasis.php
@@ -1,11 +1,13 @@
 <?php
 
 define('_ADMINEXEC_', 1);
-@include("../../GameEngine/config.php");
+include("../../Admin/admin.model.php");
+include("../../GameEngine/config.php");
 
 mysql_connect(SQL_SERVER, SQL_USER, SQL_PASS);
 mysql_select_db(SQL_DB);
 
+$admin = new adminModel();
 $database->poulateOasisdata();
 $database->populateOasis();
 $database->populateOasisUnitsLow();
