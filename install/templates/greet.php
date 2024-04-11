bb40a42a65d2e255550528f38ee3e5ae2a3b6cd4
John <juaorok@hotmail.com>
2024-04-11 19:41:18 -0300

    Update greet.php

--- install/templates/greet.php
+++ install/templates/greet.php
@@ -20,18 +20,18 @@
         raised initiated by the owners of any unlicensed content you permit your copy of this game to publish.
     </li>
 
-    <li>Neither the team that created this script nor the team that customised it to create this distribution/release
+    <li>Neither the team that created this script nor the team that customized it to create this distribution/release
         shall be responsible for any damage done to your computer/server system.
     </li>
 
     <li>All code was confirmed to be running correctly by the creation team without any visible security risk they were
-        aware of at the time the released it. Similarly for the customisation team who customised it to create this
+        aware of at the time the released it. Similarly for the customization team who customized it to create this
         distribution/release.
     </li>
 
     <li>Users are asked to review the code on their own accord and behalf.</li>
 
-    <li>Any customization to the source code are the property of each customisation's author, it is up to each author's
+    <li>Any customization to the source code are the property of each customization's author, it is up to each author's
         discretion whether to share it by including it in free open source distributions such as this one.
     </li>
 
