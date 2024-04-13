66bca5528936311ab3b597eb4f9b8d59db2937d8
John <juaorok@hotmail.com>
2024-04-13 06:56:00 -0300

    Update newsbox2.php

--- templates/News/newsbox2.php
+++ templates/News/newsbox2.php
@@ -1,5 +1,3 @@
 </center><b>Communication Ways to Buy</b></center>
 <br>
 </center>Contact me to buy Travian 4.4 script:</center>
-<br>
-</center>Contact number: 09335751872 id yahoo: gorz1872@yahoo.com</center>
