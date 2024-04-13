4f7c634303c4a3b93eec694bf6459b1aed7dd7f3
John <juaorok@hotmail.com>
2024-04-13 06:55:52 -0300

    Update general.php

--- templates/Ranking/general.php
+++ templates/Ranking/general.php
@@ -1,9 +1,10 @@
 <?php
-$tribe1 = mysql_query("SELECT `id` FROM " . TB_PREFIX . "users WHERE tribe = 1 and id>4");
-$tribe2 = mysql_query("SELECT `id` FROM " . TB_PREFIX . "users WHERE tribe = 2 and id>4");
-$tribe3 = mysql_query("SELECT `id` FROM " . TB_PREFIX . "users WHERE tribe = 3 and id>4");
+
+$tribe1 = mysql_query("SELECT `id` FROM " . TB_PREFIX . "users WHERE tribe = 1 and id > 4");
+$tribe2 = mysql_query("SELECT `id` FROM " . TB_PREFIX . "users WHERE tribe = 2 and id > 4");
+$tribe3 = mysql_query("SELECT `id` FROM " . TB_PREFIX . "users WHERE tribe = 3 and id > 4");
 $tribes = array(mysql_num_rows($tribe1), mysql_num_rows($tribe2), mysql_num_rows($tribe3));
-$users = mysql_num_rows(mysql_query("SELECT SQL_CACHE * FROM " . TB_PREFIX . "users WHERE id>4"));
+$users = mysql_num_rows(mysql_query("SELECT SQL_CACHE * FROM " . TB_PREFIX . "users WHERE id > 4"));
 
 $config = mysql_fetch_array($database->query("SELECT * FROM " . TB_PREFIX . "config LIMIT 1"));
 //$np = $database->getNatarsProgress();
