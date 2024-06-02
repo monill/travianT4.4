dc9f53a40ba9da4c79a166803154a130f64c3cfd
John <juaorok@hotmail.com>
2024-06-02 19:53:10 -0300

    Update config.php

--- Gateways/payline/config.php
+++ Gateways/payline/config.php
@@ -1,7 +1,7 @@
 <?php
 $config = array(
     'payline' => array(
-        'name' => 'درگاه پی لاین',
+        'name' => 'payline port',
         'image' => $config['server_url'] . '/Gateways/0/image.png',
         'start' => 'Gateways/payline/start.php',
         'api' => 'd947c-f5dd3-898b6-c106b-001cdc5cd48f38fd5abc865d0a61',
